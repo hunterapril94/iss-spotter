52820e591a01d6f934ae45276f2ee412d8e024bb
April Larsen <hunterapril94@gmail.com>
2021-09-24 00:31:03 +0000

    now able to get the next pass times of ISS based on ip location

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const {fetchMyIP} = require('./iss');
+const { fetchMyIP, fetchCoordinatesByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
+
+// fetchISSFlyOverTimes({ latitude: 49.2825, longitude: -123.1291 }, (err, body) => {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(body)
+//   }
+// });
+// fetchCoordinatesByIP("24.80.125.48", (error, body) => {
+//   if(error) {
+//     console.log(error)
+//   } else {
+//     console.log(body)
+//   }
+
+// })
 // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
--- iss.js
+++ iss.js
@@ -10,6 +10,7 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org?format=json', (error, response, body) => {
+    let ip = JSON.parse(body).ip;
     if (error) {
       body = null;
       callback(error, body);
@@ -19,9 +20,77 @@ const fetchMyIP = function(callback) {
       callback(Error(msg), null);
       return;
     } else {
-      callback(null, JSON.parse(body).ip);
+      callback(null, ip);
+      return ip;
+    }
+
+  });
+};
+
+const fetchCoordinatesByIP = function(ip, callback) {
+  request('https://freegeoip.app/json/' + ip, (error, response, body) => {
+    if (error) {
+      body = null;
+      callback(error, body);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+    } else {
+      let lat = JSON.parse(body).latitude;
+      let long = JSON.parse(body).longitude;
+      callback(null, {"latitude": lat, "longitude" : long});
+      return JSON.parse(body);
     }
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request('https://iss-pass.herokuapp.com/json/?lat=' + coords.latitude + "&lon=" + coords.longitude, (error, response, body) => {
+    if (error) {
+      body = null;
+      callback(error, body);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+    } else {
+      const passes = JSON.parse(body).response
+      callback(null, passes);
+      return passes;
+    }
+  });
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordinatesByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+module.exports = { fetchMyIP, fetchCoordinatesByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
